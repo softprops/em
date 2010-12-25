cb9be24bbaa1f98ad2c9038ded8a5146f4e90ff6
softprops <d.tangren@gmail.com>
2010-12-25 02:17:54 -0500

    gh

---
+++ init.el
@@ -0,0 +1,135 @@
+; "use only what you need"
+
+(defvar *emacs-load-start* (current-time))
+
+(add-to-list 'load-path "~/.emacs.d")
+
+; no #foo#
+(setq auto-save-default nil)
+
+; no foo~
+(setq backup-inhibited t)
+
+; indentation
+(setq standard-indent 2)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+
+; char-encoding
+(prefer-coding-system 'utf-8)
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+
+; whitespace
+;(global-whitespace-mode t)
+;(setq show-trailing-whitespace t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+; scala
+(add-to-list 'load-path "~/.emacs.d/vendor/scala-mode")
+(require `scala-mode-auto)
+
+; SUPER scala!!
+;; Load the ensime lisp code...
+(add-to-list 'load-path "~/.emacs.d/vendor/ensime/elisp")
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+; markdown
+(add-to-list 'load-path "~/.emacs.d/vendor/markdown-mode")
+(autoload 'markdown-mode "markdown-mode.el"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
+; js*
+; possible replacement ~> http://code.google.com/p/js2-mode/
+(autoload 'js-mode "js" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js-mode))
+
+; coffee-script
+; see https://github.com/defunkt/coffee-mode for more fixins
+(add-to-list 'load-path "~/.emacs.d/vendor/coffee-mode")
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+; ruby
+(autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files" t)
+(autoload 'run-ruby "inf-ruby" "Run an inferior Ruby process")
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.rake" . ruby-mode) auto-mode-alist))
+
+; nxhtml (for fundamental web stuffs)
+(load "~/.emacs.d/vendor/nxhtml/autostart.el")
+(setq
+  nxhtml-global-minor-mode t
+  mumamo-chunk-coloring 'submode-colored
+  nxhtml-skip-welcome t
+  indent-region-mode t
+  rng-nxml-auto-validate-flag nil
+  nxml-degraded t)
+(eval-after-load 'nxhtml
+  '(eval-after-load 'color-theme
+     (custom-set-faces
+       '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) (:background "#242424"))))
+       '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background dark)) (:background "#373736"))))
+)))
+
+; colors
+(add-to-list 'load-path "~/.emacs.d/vendor/color-theme-6.6.0")
+(require 'color-theme)
+(color-theme-initialize)
+(setq color-theme-is-global t)
+(load-file "~/.emacs.d/vendor/color-theme-github/color-theme-github.el")
+(color-theme-github)
+;(load-file "~/.emacs.d/vendor/twilight-emacs/color-theme-twilight.el")
+;(color-theme-twilight)
+
+; buffers
+
+(iswitchb-mode 1)
+(setq iswitchb-buffer-ignore '("^ " "*Buffer"))
+(setq iswitchb-buffer-ignore '("^\\*"))
+(setq iswitchb-default-method 'samewindow)
+; use left & right keys to navigate buffers
+(defun iswitchb-local-keys ()
+      (mapc (lambda (K)
+	      (let* ((key (car K)) (fun (cdr K)))
+    	        (define-key iswitchb-mode-map (edmacro-parse-keys key) fun)))
+	    '(("<right>" . iswitchb-next-match)
+	      ("<left>"  . iswitchb-prev-match)
+	      ("<up>"    . ignore             )
+	      ("<down>"  . ignore             ))))
+    (add-hook 'iswitchb-define-mode-map-hook 'iswitchb-local-keys)
+; short cut for iswitch-buffer
+(global-set-key (kbd "C-b") 'iswitchb-buffer)
+
+; tab completion
+; http://www.emacswiki.org/cgi-bin/wiki/TabCompletion
+(global-set-key [(tab)] 'smart-tab)
+(defun smart-tab ()
+  "This smart tab is minibuffer compliant: it acts as usual in
+    the minibuffer. Else, if mark is active, indents region. Else if
+    point is at the end of a symbol, expands it. Else indents the
+    current line."
+  (interactive)
+  (if (minibufferp)
+      (unless (minibuffer-complete)
+        (dabbrev-expand nil))
+    (if mark-active
+        (indent-region (region-beginning)
+                       (region-end))
+      (if (looking-at "\\_>")
+          (dabbrev-expand nil)
+        (indent-for-tab-command)))))
+
+; tell all via https://github.com/stevej/emacs/blob/master/init.el#L53-55
+(message "Emacs loaded in  %ds" (destructuring-bind (hi lo ms) (current-time)
+                             (- (+ hi lo) (+ (first *emacs-load-start*) (second
+                             *emacs-load-start*)))))
